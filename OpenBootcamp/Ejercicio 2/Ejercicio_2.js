56493882e95bda7c72fb5832e669779a8018f433
Gazer808 <96377170+Gazer808@users.noreply.github.com>
2022-05-10 17:08:28 +0200

    Add files via upload

---
+++ OpenBootcamp/Ejercicio 2/Ejercicio_2.js
@@ -0,0 +1,19 @@
+var nombre = "Kilian";
+var edad = 20;
+var eresProgramador = true;
+var nacimiento = new Date(2001, 5, 9)
+var libroFavorito = {
+    titulo: "Noname",
+    autor: "Nobody",
+    fecha: new Date(2222, 11, 11)
+}
+
+const lista = [
+    nombre,
+    edad,
+    eresProgramador,
+    nacimiento,
+    libroFavorito
+]
+
+console.log(lista);
